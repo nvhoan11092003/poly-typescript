bc91bba26eecc365c424a222b695b73673a5ac55
nvhoan11092003 <nvhoan11092003@gmail.com>
2023-04-23 08:25:51 +0700

    21432432

---
+++ poly-typescript/src/models.ts
@@ -0,0 +1,18 @@
+import * as yup from "yup";
+export interface Iproduct {
+  name: string;
+  trademark: string;
+  description: string;
+  price: number;
+  origin: string;
+}
+
+export const updateSchema = yup.object({
+  name: yup.string().required("trường dữ liệu bắt buộc"),
+  trademark: yup.string().required("trường dữ liệu bắt buộc"),
+  description: yup.string().required("trường dữ liệu bắt buộc"),
+  price: yup.number().required("trường dữ liệu bắt buộc"),
+  origin: yup.string().required("trường dữ liệu bắt buộc"),
+});
+
+export type updateForm = yup.InferType<typeof updateSchema>;
